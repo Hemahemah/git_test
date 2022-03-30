87eb5e9458f64aba7068e47c2f31e98a61732f9f
zlh <2818323885@qq.com>
2022-03-30 10:45:40 +0800

    second commit

--- src/main/java/com/example/git_test/GitTestApplication.java
+++ src/main/java/com/example/git_test/GitTestApplication.java
@@ -9,6 +9,7 @@ public class GitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
         System.out.println("git test");
+        System.out.println("hello github");
     }
 
 }
